2e5b9523df91112339287bb8a7ac26bbbd1ac52b
Arpit Agarwal <arpit.agarwal181@gmail.com>
2020-12-14 03:11:07 +0530

    update models

--- rera/models.py
+++ rera/models.py
@@ -2,28 +2,31 @@ from django.db import models
 
 
 class Organization(models.Model):
-    name = models.CharField(max_length=512)
-    org_type = models.CharField(max_length=128)
-    address = models.TextField()
-    number = models.CharField(max_length=16)
+    name = models.CharField(max_length=512, null=True)
+    org_type = models.CharField(max_length=128, null=True)
+    address = models.TextField(null=True)
+    number = models.CharField(max_length=16, null=True)
 
 
 class Application(models.Model):
     application_no = models.CharField(max_length=32, primary_key=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    name = models.TextField()
 
 
 class RealEstateAgent(models.Model):
-    application_no = models.CharField(max_length=32)
+    application_no = models.CharField(max_length=32, null=True)
     pan = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=512)
-    org_type = models.CharField(max_length=128)
+    name = models.CharField(max_length=512, null=True)
+    org_type = models.CharField(max_length=128, null=True)
     address = models.TextField()
-    office_number = models.CharField(max_length=16)
-    mobile = models.CharField(max_length=16)
-    email = models.CharField(max_length=128)
+    office_number = models.CharField(max_length=16, null=True)
+    mobile = models.CharField(max_length=16, null=True)
+    email = models.CharField(max_length=128, null=True)
     applications = models.ManyToManyField(Application, related_name='applications', through='RealEstateAgentContract')
 
 
 class RealEstateAgentContract(models.Model):
     agent = models.ForeignKey(RealEstateAgent, on_delete=models.CASCADE)
-    application_no = models.ForeignKey(Application, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=128, null=True)
