b110e9ae90335a02dae557f9922dfe17eafeb41a
Arpit Agarwal <arpit.agarwal181@gmail.com>
2020-12-14 02:18:02 +0530

    rename settings package to config

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rera',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'maha_rera.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'maha_rera.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
